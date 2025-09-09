a989145f3bd47da6a6a383e4036e6c9bbd5976ca
clairinechristabel <clairine.christabel@ui.ac.id>
2025-09-10 00:31:11 +0700

    commit 3

--- dudushop/settings.py
+++ dudushop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+2f4uj+lx4=^l4i^4em#nr2na2&-%=x5zk26a+&5m1p6w-#9xg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "clairine-christabel-dudushop.pbp.cs.ui.ac.id"]
 
 
 # Application definition
