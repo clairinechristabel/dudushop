41714bf913847033fcc6ebea3ae0326c1c7ed6c2
clairinechristabel <clairine.christabel@ui.ac.id>
2025-10-01 11:01:06 +0700

    Tugas 5

--- main/forms.py
+++ main/forms.py
@@ -5,3 +5,10 @@ class ShopForm(ModelForm):
     class Meta:
         model = Shop
         fields = ["name", "price", "description", "thumbnail", "category", "is_featured"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                "class": "w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-pink-400 focus:border-pink-400"
+            })
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from main.views import show_main
 from main.views import show_main, show_xml, show_json, show_xml_by_id, show_json_by_id, add_product, detail_product
-from main.views import register, login_user, logout_user
+from main.views import register, login_user, logout_user, edit_product, delete_product
 
 
 app_name = 'main'
@@ -17,4 +17,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('news/<uuid:id>/edit', edit_product, name='edit_product'),
+    path('news/<uuid:id>/delete', delete_product, name='delete_product'),
 ]
